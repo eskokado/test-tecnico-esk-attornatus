a6048930418eb05729933d986f2b99ab0f4be3d9
eskokado <eskokado@gmail.com>
2023-09-25 18:54:40 -0300

    feat: create entity endereco

---
+++ src/main/java/com/eskokado/attornatus/api/domain/Endereco.java
@@ -0,0 +1,24 @@
+package com.eskokado.attornatus.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String logradouro;
+    private String cep;
+    private String numero;
+    private String cidade;
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+}
