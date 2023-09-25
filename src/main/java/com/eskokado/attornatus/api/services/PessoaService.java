28f2e24fe775b916b2a382d214a09a4dfa22938c
eskokado <eskokado@gmail.com>
2023-09-25 19:07:46 -0300

    feat: service editarPessoa

--- src/main/java/com/eskokado/attornatus/api/services/PessoaService.java
+++ src/main/java/com/eskokado/attornatus/api/services/PessoaService.java
@@ -5,11 +5,23 @@ import com.eskokado.attornatus.api.repositories.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PessoaService {
     @Autowired
     private PessoaRepository pessoaRepository;
     public Pessoa criarPessoa(Pessoa pessoa) {
         return pessoaRepository.save(pessoa);
+    }
+
+    public Pessoa editarPessoa(Long id, Pessoa pessoa) {
+        Optional<Pessoa> pessoaExistente = pessoaRepository.findById(id);
+        if (pessoaExistente.isPresent()) {
+            pessoa.setId(id);
+            return pessoaRepository.save(pessoa);
+        } else {
+            return null;
+        }
     }
 }
