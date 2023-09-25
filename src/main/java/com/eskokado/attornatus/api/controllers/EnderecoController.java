cc71d6f4ccab06c6658f23562d621a37c9a01f19
eskokado <eskokado@gmail.com>
2023-09-25 19:43:08 -0300

    feat: controller listarEnderecosDaPessoa

--- src/main/java/com/eskokado/attornatus/api/controllers/EnderecoController.java
+++ src/main/java/com/eskokado/attornatus/api/controllers/EnderecoController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/enderecos")
 public class EnderecoController {
@@ -20,5 +22,10 @@ public class EnderecoController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/pessoa/{pessoaId}")
+    public List<Endereco> listarEnderecosDaPessoa(@PathVariable Long pessoaId) {
+        return enderecoService.listarEnderecosDaPessoa(pessoaId);
     }
 }
