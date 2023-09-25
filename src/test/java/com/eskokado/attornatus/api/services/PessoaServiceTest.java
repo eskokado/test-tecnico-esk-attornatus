6d7e1e676270b3fd7a743b1797288d0dfad34f29
eskokado <eskokado@gmail.com>
2023-09-25 18:56:51 -0300

    test: service criarPessoa

---
+++ src/test/java/com/eskokado/attornatus/api/services/PessoaServiceTest.java
@@ -0,0 +1,38 @@
+package com.eskokado.attornatus.api.services;
+
+import com.eskokado.attornatus.api.domain.Pessoa;
+import com.eskokado.attornatus.api.repositories.PessoaRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PessoaServiceTest {
+
+    @InjectMocks
+    private PessoaService pessoaService;
+
+    @Mock
+    private PessoaRepository pessoaRepository;
+
+    @Test
+    public void testCriarPessoa() {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("João");
+        pessoa.setDataNascimento(LocalDate.of(1990, 5, 15));
+
+        Mockito.when(pessoaRepository.save(Mockito.any(Pessoa.class))).thenReturn(pessoa);
+
+        Pessoa pessoaCriada = pessoaService.criarPessoa(pessoa);
+
+        assertEquals("João", pessoaCriada.getNome());
+        assertEquals(LocalDate.of(1990, 5, 15), pessoaCriada.getDataNascimento());
+    }
+}
