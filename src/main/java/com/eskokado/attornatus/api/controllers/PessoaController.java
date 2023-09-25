bc0fab19c95d593073dfdf165d778cdbbba80c5a
eskokado <eskokado@gmail.com>
2023-09-25 19:25:39 -0300

    feat: controller listarPessoa

--- src/main/java/com/eskokado/attornatus/api/controllers/PessoaController.java
+++ src/main/java/com/eskokado/attornatus/api/controllers/PessoaController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/pessoas")
 public class PessoaController {
@@ -39,4 +41,8 @@ public class PessoaController {
         }
     }
 
+    @GetMapping
+    public List<Pessoa> listarPessoas() {
+        return pessoaService.listarPessoas();
+    }
 }
