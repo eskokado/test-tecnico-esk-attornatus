3d17c9ee7b0a2dfec08b69759020e18884cc33d2
eskokado <eskokado@gmail.com>
2023-09-25 19:26:23 -0300

    feat: service criarEnderecoParaPessoa

---
+++ src/main/java/com/eskokado/attornatus/api/services/EnderecoService.java
@@ -0,0 +1,28 @@
+package com.eskokado.attornatus.api.services;
+
+import com.eskokado.attornatus.api.domain.Endereco;
+import com.eskokado.attornatus.api.domain.Pessoa;
+import com.eskokado.attornatus.api.repositories.EnderecoRepository;
+import com.eskokado.attornatus.api.repositories.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EnderecoService {
+    @Autowired
+    private PessoaRepository pessoaRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    public Endereco criarEnderecoParaPessoa(Long pessoaId, Endereco endereco) {
+        Optional<Pessoa> pessoa = pessoaRepository.findById(pessoaId);
+        if (pessoa.isPresent()) {
+            endereco.setPessoa(pessoa.get());
+            return enderecoRepository.save(endereco);
+        } else {
+            return null;
+        }
+    }
+
+}
