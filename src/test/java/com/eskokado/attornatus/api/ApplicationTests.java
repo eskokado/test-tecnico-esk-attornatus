0d70081e32b3b3cc4c1bc91f3ac36ddc62494e88
eskokado <eskokado@gmail.com>
2023-09-25 18:55:48 -0300

    feat: create app test

---
+++ src/test/java/com/eskokado/attornatus/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskokado.attornatus.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
